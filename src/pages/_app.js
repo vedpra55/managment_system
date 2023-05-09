5a2f55c6761a1ff2cbdd7b7a773a928cd13094ed
vedpra55 <68733473+vedpra55@users.noreply.github.com>
2023-05-09 19:10:30 +0530

    client rend

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,20 @@
 import Header from "@/components/header";
 import "@/styles/globals.css";
+import { useEffect, useState } from "react";
 
 import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
+
+  const [clinet, setClient] = useState(false);
+
+  useEffect(() => {
+    setClient(true);
+  }, []);
+
+  if (!clinet) return;
+
   return (
     <main>
       <Toaster />
