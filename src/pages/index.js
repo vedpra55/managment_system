7c01643d06fdcf4d40c0a5e97e9f46aacb953ae4
vedpra55 <68733473+vedpra55@users.noreply.github.com>
2023-05-11 13:44:02 +0530

    new update 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,18 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Sidebar from "@/components/Sidebar/sidebar";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/work-order");
+  }, []);
+
   return (
     <>
       <Head></Head>
